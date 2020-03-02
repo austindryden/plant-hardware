b70caf1b81c421aba6d6507586a0e2f7367f8fd4
Austin Dryden <austinisnorobot@gmail.com>
2020-03-02 18:34:12 -0500

    fixes moisture sensor readings.

--- gardengnome/gardengnome.ino
+++ gardengnome/gardengnome.ino
@@ -1,20 +1,19 @@
 #include <SimpleDHT.h>
-#include <I2CSoilMoistureSensor.h> 
-#include <Wire.h> 
-I2CSoilMoistureSensor sensor(0x60); 
+ 
 // for DHT22, 
 //      VCC: 5V or 3V
 //      GND: GND
 //      DATA: 2
 int pinDHT22 = 2;
 int LDRpin = A0;
+int moisturePin =A1;
 SimpleDHT22 dht22(pinDHT22);
 int LDRValue = 0;     // result of reading the analog pin
-
+int moistureValue = 0;
+const int AirValue = 575;
+const int WaterValue = 340; // reference readings for dry air, and glass of water
 void setup() {
   Serial.begin(9600);
-  Wire.begin(); 
-  sensor.begin(); // reset sensor 
   delay(1000); // give some time to boot up 
 //  Serial.print("I2C Soil Moisture Sensor Address: "); 
 //  Serial.println(sensor.getAddress(),HEX); 
@@ -39,12 +38,13 @@ void loop() {
     Serial.print("Read DHT22 failed, err="); Serial.println(err);delay(2000);
     return;
   }
-  LDRValue = analogRead(LDRpin); // read the value from the LDR
+  LDRValue = analogRead(LDRpin);// read the value from the LDR
+  moistureValue = analogRead(moisturePin);
   //Serial.print("{ timestamp: time");
   Serial.print("{temp:");
   Serial.print((float)temperature); Serial.print(", humidity: ");
   Serial.print((float)humidity); Serial.print(", soilmoisture: ");
-  Serial.print(sensor.getCapacitance()); //read capacitance register 
+  Serial.print(moistureValue); //read capacitance register 
   Serial.print(", lightsensor:"); 
   Serial.print(LDRValue);Serial.print("}");
   Serial.println("");// print the value to the serial port
