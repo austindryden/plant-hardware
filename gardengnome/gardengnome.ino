625c662e287d5a4aedce27562ea6ca69b9e7adc2
Austin Dryden <austinisnorobot@gmail.com>
2020-03-12 14:32:27 -0400

    comments

--- gardengnome/gardengnome.ino
+++ gardengnome/gardengnome.ino
@@ -1,9 +1,5 @@
 #include <SimpleDHT.h>
- 
-// for DHT22, 
-//      VCC: 5V or 3V
-//      GND: GND
-//      DATA: 2
+
 int pinDHT22 = 2;
 int LDRpin = A0;
 int moisturePin =A1;
@@ -14,8 +10,8 @@ SimpleDHT22 dht22(pinDHT22);
 int LDRValue = 0;     // result of reading the analog pin
 int moistureValue = 0;
 const int AirValue = 575;
-const int WaterValue = 340;// reference readings for dry air, and glass of water
-const int lowWater = 496;
+const int WaterValue = 340; // reference readings for dry air, and glass of water
+const int lowWater = 496; // break points for moisture readings. goldilocks zone in between.
 const int highWater = 418;
 void setup() {
   Serial.begin(9600);
@@ -23,22 +19,9 @@ void setup() {
   pinMode(greenPin, OUTPUT);
   pinMode(redPin, OUTPUT);
   delay(1000); // give some time to boot up 
-//  Serial.print("I2C Soil Moisture Sensor Address: "); 
-//  Serial.println(sensor.getAddress(),HEX); 
-//  Serial.print("Sensor Firmware version: "); 
-//  Serial.println(sensor.getVersion(),HEX); 
-//  Serial.println(); 
-
 }
 
 void loop() {
-  // start working...
-//  Serial.println("=================================");
-//  Serial.println("Sample DHT22...");
-  
-  // read without samples.
-  // @remark We use read2 to get a float data, such as 10.1*C
-  //    if user doesn't care about the accurate data, use read to get a byte data, such as 10*C.
   float temperature = 0;
   float humidity = 0;
   int err = SimpleDHTErrSuccess;
@@ -56,17 +39,19 @@ void loop() {
   Serial.print(", lightsensor:"); 
   Serial.print(LDRValue);Serial.print("}");
   Serial.println("");// print the value to the serial port
+  //Turn on red light because the plant is too dry
   if(moistureValue > lowWater){
       digitalWrite(redPin, HIGH);
       digitalWrite(bluePin,LOW);
       digitalWrite(greenPin,LOW);
-      
-   }
+    }
+//Turn on blue light because the plant is too wet    
    if(moistureValue < highWater){
       digitalWrite(redPin, LOW);
       digitalWrite(bluePin,HIGH);
       digitalWrite(greenPin,LOW);
    }
+//Turn on green light because the plant is just right
    if(moistureValue > highWater && moistureValue < lowWater){
       digitalWrite(redPin, LOW);
       digitalWrite(bluePin,LOW);
--- index.js
+++ index.js
@@ -1,15 +1,21 @@
+const db = require('../backend/connection');
 const SerialPort = require('serialport');
 const Readline = require('@serialport/parser-readline');
 const port = new SerialPort('/dev/ttyACM0', { baudRate: 9600 });
-
 const parser = port.pipe(new Readline({ delimiter: "\n" }));
+let dataArr = [];
+let counter = 0;
 // Read the port data
 
 port.on("open", () => {
   console.log('serial port open');
 });
 
-parser.on('data', data =>{
+parser.on('data', async data =>{
   console.log('got word from arduino:', data);
-  
+  counter +=1;
+  dataArr.push(data);
+  if (counter >= 12){
+    counter = 0;  
+  }
 });
